25711d47a65e265d9c0f773a60b00f0cc2b7a68f
Guilherme Henrique <gh.dourado77@gmail.com>
2024-01-26 15:47:44 -0300

    feat: initial project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/Header";
+import { ListTarefa } from "@/components/ListTarefa";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <ListTarefa />
+    </>
+  );
+}
